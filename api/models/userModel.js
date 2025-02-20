bfe7d340a8153cbb39a36d2693c41b148c066fe9
Shukra Tamang <shukratamang.npl@gmail.com>
2025-02-20 11:46:17 +1100

    create api for google OAuth

--- api/models/userModel.js
+++ api/models/userModel.js
@@ -16,6 +16,11 @@ const userSchema = new mongoose.Schema(
       type: String,
       required: true,
     },
+    avatar: {
+      type: String,
+      default:
+        "https://www.utsmotorsports.com/wp-content/uploads/2019/03/kisspng-computer-icons-user-image-person-silhouette-my-svg-png-icon-free-download-419940-onlinewe-5b6512e70b0b21.9641097515333506310452.png",
+    },
   },
   {
     timestamps: true,
--- api/routes/authRoute.js
+++ api/routes/authRoute.js
@@ -1,9 +1,11 @@
 import express from "express";
-import { signin, signup } from "../controllers/authController.js";
+import { google, signin, signup } from "../controllers/authController.js";
 
 const router = express.Router();
 
 router.post("/signup", signup);
 router.post("/signin", signin);
+// google route
+router.post("/google", google);
 
 export default router;
