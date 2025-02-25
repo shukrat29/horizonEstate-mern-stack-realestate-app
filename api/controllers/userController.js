4b3784f2edf78244d048875c95f55f9ed81f15b1
Shukra Tamang <shukratamang.npl@gmail.com>
2025-02-25 12:39:29 +1100

    create get user listings api route

--- api/controllers/userController.js
+++ api/controllers/userController.js
@@ -1,6 +1,8 @@
+import Listing from "../models/listingModel.js";
 import User from "../models/userModel.js";
 import { errorHandler } from "../utils/error.js";
 
+// update user
 export const updateUser = async (req, res, next) => {
   if (req.user.id !== req.params.id)
     return next(errorHandler(401, "You can only update your own account"));
@@ -40,5 +42,19 @@ export const deleteUser = async (req, res, next) => {
     res.status(200).json("User has been deleted");
   } catch (error) {
     next(error);
+  }
+};
+
+// get user listings
+export const getUserListings = async (req, res, next) => {
+  if (req.user.id === req.params.id) {
+    try {
+      const listings = await Listing.find({ userRef: req.params.id });
+      res.status(200).json(listings);
+    } catch (error) {
+      next(error);
+    }
+  } else {
+    return next(errorHandler(401, "You can only view your own listings!"));
   }
 };
